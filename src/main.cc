60fec50ca1bee41e8b2f05e265e89a57c85272df
Arthur Douillard <douill_a@epita.fr>
2016-07-06 22:51:07 +0200

    Copy video.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include "options/options.hh"
 
@@ -8,8 +10,24 @@ namespace bpo = boost::program_options;
 int main(int argc, char** argv)
 {
   Options opt = options_parser(argc, argv);
-  std::cout << opt.video  << std::endl;
-  for (auto f : opt.filter)
-    std::cout << f << std::endl;
+
+  // Get input video.
+  cv::VideoCapture cap(opt.video);
+  // New video frames.
+  std::vector<cv::Mat> frames;
+
+  if (!cap.isOpened())
+  {
+    std::cerr << "Error when reading video." << std::endl;
+    exit(1);
+  }
+
+  // Copy original video to the new one.
+  cv::Mat frame;
+  while (true)
+  {
+    cap >> frame;
+    frames.push_back(frame);
+  }
 }
 
