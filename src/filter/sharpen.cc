9dd3e6cb7dece59ff3d0293fecfafbaf725516d7
Arthur Douillard <douill_a@epita.fr>
2016-07-07 19:25:49 +0200

    Implem sharpen convolution.

---
+++ src/filter/sharpen.cc
@@ -0,0 +1,60 @@
+#include "sharpen.hh"
+
+namespace filters
+{
+  Sharpen::Sharpen(tbb::filter::mode mode,
+                   frame_iterator first, frame_iterator last)
+    : ModelFilter(mode, "sharpen")
+    , first_(first)
+    , last_(last)
+  {}
+
+  void* Sharpen::operator()(void* ptr)
+  {
+    // end the pipeline after the last image
+    if (first_ == last_)
+      return nullptr;
+
+    img_ = (ptr != nullptr) ? *(static_cast<cv::Mat*>(ptr)) : *first_;
+
+    std::vector<std::vector<float>> kernel = {
+      { 0, -1, 0 },
+      { -1, 5, -1 },
+      { -1, 0, -1 }
+    }
+
+    cv::Mat copy = img_.clone();
+
+    for (int i = 1; i < img_.rows - 1; i++)
+    {
+      for (int j = 1; j < img_.cols - 1; j++)
+      {
+        int acc_r = 0;
+        int acc_g = 0;
+        int acc_b = 0;
+
+        for (int row = 0; row < kernel.size(); row++)
+        {
+          for (int pos = 0; pos < kernel[row].size(); pos++)
+          {
+            int elt = kernel[row][pos];
+            auto in = img_.at<cv::Vec3b>(i - row, j - pos);
+
+            acc_r += elt * in[2];
+            acc_g += elt * in[1];
+            acc_b += elt * in[0];
+          }
+        }
+
+        auto& out = copy.at<cv::Vec3b>(i, j);
+        out[2] = acc_r < 0 ? 0 : acc_r;
+        out[1] = acc_g < 0 ? 0 : acc_g;
+        out[0] = acc_b < 0 ? 0 : acc_b;
+      }
+    }
+    ++first_;
+
+    return &copy;
+  }
+
+}
---
+++ src/filter/sharpen.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <tbb/tbb.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "modelfilter.hpp"
+
+namespace filters
+{
+  using frame_iterator = std::vector<cv::Mat>::iterator;
+
+  class Sharpen : public ModelFilter
+  {
+    public:
+      Sharpen(tbb::filter::mode, frame_iterator first, frame_iterator last);
+      void *operator()(void* image);
+
+    private:
+      frame_iterator first_;
+      frame_iterator last_;
+      cv::Mat img_;
+  };
+
+} // filters
