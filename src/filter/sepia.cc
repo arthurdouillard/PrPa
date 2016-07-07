fef4dad6b27aab7e3dca6a9a419e89dc94d47fc2
Loic Banet <banet_l@epita.fr>
2016-07-07 19:35:30 +0200

    fix sepia

--- src/filter/sepia.cc
+++ src/filter/sepia.cc
@@ -27,15 +27,15 @@ namespace filters
       for (int j = 0; j < img_.cols; j++)
       {
         cv::Vec3b &current_img = img_.at<cv::Vec3b>(i, j);
-        unsigned char blue = current_img[0]; // B
+        unsigned char blue = current_img[2]; // B
         unsigned char green = current_img[1]; // G
-        unsigned char red = current_img[2]; // R
+        unsigned char red = current_img[0]; // R
 
-        current_img[0] = std::min(255,
+        current_img[2] = std::min(255,
               (int) std::lround(red * 0.393 + green * 0.769 + blue * 0.189));
         current_img[1] = std::min(255,
               (int) std::lround(red * 0.349 + green * 0.689 + blue * 0.168));
-        current_img[2] = std::min(255,
+        current_img[0] = std::min(255,
               (int) std::lround(red * 0.272 + green * 0.534 + blue * 0.131));
       }
     }
